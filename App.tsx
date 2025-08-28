732541205fe54a6e4a355930645ffe6ebca323fb
Oleh Polishchuk <oleg.polischuks@gmail.com>
2025-08-28 15:06:30 -0700

    Added payments debug

--- App.tsx
+++ App.tsx
@@ -73,12 +73,12 @@ const AppContent: React.FC = () => {
 
   useEffect(() => {
     // Initialize RevenueCat
+    Purchases.setLogLevel(LOG_LEVEL.DEBUG);
     if (Platform.OS === 'ios') {
        Purchases.configure({apiKey: "appl_KopTWcANzpTAMEriDmzPeFhFiVu"});
     } else if (Platform.OS === 'android') {
        Purchases.configure({apiKey: "goog_FzScAUIKXLprLfvKBhyYjdmLHvJ"});
     } else if (Platform.OS !== 'web') {
-      Purchases.setLogLevel(LOG_LEVEL.DEBUG);
       Purchases.configure({
         apiKey: "goog_FzScAUIKXLprLfvKBhyYjdmLHvJ"
       });
